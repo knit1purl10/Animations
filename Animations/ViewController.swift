f071fff32eadd3d2e4230253e5833d5ffd8b0808
Olivia Hon <olivia.hon@gmail.com>
2018-10-30 18:37:21 -0400

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Olivia Hon on 10/30/18.
+//  Copyright © 2018 Olivia Hon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
